c3b260394796839830978cfe20abe0ca58e3e8f6
Moatasem <m3tsmsh@gmail.com>
2021-12-12 07:25:50 +0200

    edits in action files

---
+++ src/actions/shared.js
@@ -0,0 +1,18 @@
+import { getInitialData } from "../../API";
+import getQuestions from './getQuestions'
+import getUsers from "getUsers"
+import authedUser, { AUTHED_USER } from "./authedUser";
+
+
+AUTHED_USER='sarahedo'
+
+export function handleInitialData () {
+    return (dispatch) => {
+      return getInitialData()
+        .then(({ users, questions }) => {
+          dispatch(getUsers(users))
+          dispatch(getQuestions(questions))
+          dispatch(authedUser(AUTHED_ID))
+        })
+    }
+  } 
